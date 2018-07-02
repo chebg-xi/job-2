015fa732d161de8417a81410b51deba821cdb836
chebg-xi <40747802+chebg-xi@users.noreply.github.com>
2018-07-02 10:46:33 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 10:40:19 2018
+
+@author: Administrator
+"""
+
+a=['28','22','23','20','19','22','29']
+f=23
+str(f)
+print(a[0],a[1],a[2],"星期三是："+str(f),a[4],a[5],a[6])
